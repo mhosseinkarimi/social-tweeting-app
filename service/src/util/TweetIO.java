2f14aa551c89174187ad69e08d01065eba91e5c2
Hossein <mhusseinkarimi@gmail.com>
2021-12-16 20:08:42 +0330

    Update service.util.TweetIO.java

--- service/src/util/TweetIO.java
+++ service/src/util/TweetIO.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -45,6 +46,24 @@ public class TweetIO {
     }
 
     /**
+     * Returns the Hashset of the usernames of the retweeters.
+     *
+     * @param tweetID The retweeted tweet's ID
+     * @return Hashset<String> username of retweeters
+     */
+    public HashSet<String> getRetweets(String tweetID) {
+        return retweets.getOrDefault(tweetID, null);
+    }
+
+    /**
+     * Returns a Hashset of the usernames associated with likes of a tweet.
+     * @param tweetID The tweet's ID
+     * @return  Hashset<String> of liksers' usernames.
+     */
+    public HashSet<String> getLikes(String tweetID){
+        return likes.getOrDefault(tweetID, null);
+    }
+    /**
      * Searches through the tweets' usernames
      *
      * @param username The username of the publisher of the tweet
@@ -76,6 +95,17 @@ public class TweetIO {
         }
 
         return likedTweetsID;
+    }
+
+    public ArrayList<String> searchOnChildTweets(String tweetID) {
+        ArrayList<String> childTweets = new ArrayList<>();
+        for (Tweet tweet : tweets) {
+            if (tweet.getParentTweetID() != null && tweet.getParentTweetID().equals(tweetID)) {
+                childTweets.add(tweet.getTweetID());
+            }
+        }
+
+        return childTweets;
     }
 
     /**
@@ -115,9 +145,18 @@ public class TweetIO {
      * @param retweeterUsername The username of the account retweeting
      */
     public void removeRetweet(String tweetID, String retweeterUsername) {
-        if (retweets.containsKey(tweetID) && retweets.get(tweetID).contains(retweeterUsername)) {
+        if (retweets.containsKey(tweetID)) {
             retweets.get(tweetID).remove(retweeterUsername);
         }
+    }
+
+    /**
+     * Removes all the retweets associated with a tweet
+     *
+     * @param tweetID The tweet's ID
+     */
+    public void removeAllRetweets(String tweetID) {
+        retweets.remove(tweetID);
     }
 
     /**
@@ -131,7 +170,7 @@ public class TweetIO {
         if (likedTweet.getLikesNumber() == 0) {
             likes.put(tweetID, new HashSet<>());
         }
-        if (likes.containsKey(tweetID) &&!likes.get(tweetID).contains(likerUsername)) {
+        if (likes.containsKey(tweetID) && !likes.get(tweetID).contains(likerUsername)) {
             likes.get(tweetID).add(likerUsername);
             likedTweet.changeLikeNumber(1);
         }
@@ -139,24 +178,70 @@ public class TweetIO {
 
     /**
      * Removes a like from the given tweet
-     * @param tweetID ID of the tweet
+     *
+     * @param tweetID       ID of the tweet
      * @param likerUsername Username of the liker
      */
-    public void removeLike(String tweetID, String likerUsername){
+    public void removeLike(String tweetID, String likerUsername) {
         Tweet likedTweet = searchOnID(tweetID);
-        if (likes.containsKey(tweetID) && !likes.get(tweetID).contains(likerUsername)){
+        if (likes.containsKey(tweetID) && !likes.get(tweetID).contains(likerUsername)) {
             likes.get(tweetID).remove(likerUsername);
             likedTweet.changeLikeNumber(-1);
         }
     }
 
     /**
+     * Removes all the likes associated with a tweet
+     *
+     * @param tweetID The tweet's ID
+     */
+    public void removeAllLikes(String tweetID) {
+        likes.remove(tweetID);
+    }
+
+    /**
+     * Adds a reply to a given tweet
+     *
+     * @param tweetID         Replied tweet ID
+     * @param replyText       Content of the reply
+     * @param replierUsername Username of the replier
+     */
+    public void addReply(String tweetID, String replyText, String replierUsername) {
+        Tweet reply = new Tweet(replierUsername, replyText, tweetID, LocalDateTime.now());
+        addTweet(reply);
+    }
+
+    /**
+     * Returns the String representation of retweets
+     * @param tweetID  retweeted tweet ID
+     * @param username Username of the retweeter
+     * @return String representation of retweet
+     */
+    public String retweetString(String tweetID, String username){
+        return "@" + username + " retweeted " + searchOnID(tweetID);
+    }
+
+    /**
+     * Prints the retweet
+     *
+     * @param tweetID retweeted tweet ID
+     * @param username Username of the retweeter
+     */
+    public void printRetweet(String tweetID, String username) {
+        if (retweets.containsKey(tweetID) && retweets.get(tweetID).contains(username)) {
+            System.out.println(retweetString(tweetID, username));
+            System.out.println("");
+        }
+    }
+
+    /**
      * Prints all the tweets in the tweet collection. Don't use in implementation. Just for testing.
      */
-    public void printAllTweets(){
+    public void printAllTweets() {
         System.out.println("================Tweets=============");
-        for(Tweet tweet : tweets){
+        for (Tweet tweet : tweets) {
             System.out.println(tweet);
+            System.out.println("");
         }
     }
 }
