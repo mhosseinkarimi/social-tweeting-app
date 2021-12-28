99ce7f2350e743ebaa8160844f9e90e2ee96bbf3
mohammad ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2021-12-27 21:19:45 -0800

    added deletefollower method

--- service/src/observer/Observer.java
+++ service/src/observer/Observer.java
@@ -2,9 +2,10 @@ package observer;
 
 import util.Account;
 import util.AccountIOInterface;
-
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Observer class Handles follow/unfollow service and prints tweets
@@ -13,7 +14,7 @@ import java.util.ArrayList;
  */
 public class Observer implements AccountIOInterface {
 
-    private String path = "";
+    private String path = "C:\\Users\\mohammad\\Desktop\\project_ap\\users";
 
     /**
      * getFollowers returns followers
@@ -26,12 +27,17 @@ public class Observer implements AccountIOInterface {
         Object Temp = null;
         //account1
         Account account1 = null;
-        //reads file
-        Temp = objectFileReader(path + "/" + username + ".bin");
-        //casting
-        account1 = (Account) Temp;
-        //returns followers
-        return account1.getFollowers();
+        //checks username
+        if(isExists(username)) {
+            //reads file
+            Temp = objectFileReader(path + "\\" + username + ".bin");
+            //casting
+            account1 = (Account) Temp;
+            //returns followers
+            return account1.getFollowers();
+        }
+        else
+            return null;
     }
 
     /**
@@ -44,13 +50,18 @@ public class Observer implements AccountIOInterface {
         //temp object1
         Object Temp = null;
         //account1
-        //reads file
         Account account1 = null;
-        Temp = objectFileReader(path + "/" + username + ".bin");
-        //casting
-        account1 = (Account) Temp;
-        //returns followings
-        return account1.getFollowings();
+        //checks username
+        if(isExists(username)) {
+            //reads file
+            Temp = objectFileReader(path + "\\" + username + ".bin");
+            //casting
+            account1 = (Account) Temp;
+            //returns followings
+            return account1.getFollowings();
+        }
+        else
+            return null;
     }
 
     /**
@@ -60,39 +71,52 @@ public class Observer implements AccountIOInterface {
      * @override
      */
     public void Follow(String username1, String username2){
-        //temp object1
-        Object Temp1 = null;
-        //temp object2
-        Object Temp2 = null;
-        //account1
-        Account account1 = null;
-        //account2
-        Account account2 = null;
-        //reads file
-        Temp1 = objectFileReader(path + "/" + username1 + ".bin");
-        //casting
-        account1 = (Account) Temp1;
-        //checks user1 state
-        if(account1.isSignedIn()){
-
-            //reads file
-            Temp2 = objectFileReader(path + "/" + username2 + ".bin");
-            //casting
-            account2 = (Account) Temp2;
-            //add following for user1
-            account1.addFollowing(username2);
-            //add follower for user2
-            account2.addFollower(username1);
-            //writes user1
-            objectFileWriter(path + "/" + username1 + ".bin", account1);
-            //writes user2
-            objectFileWriter(path + "/" + username2 + ".bin", account2);
-
+        //checks username1
+        if(!isExists(username1)){
+            //message
+            System.out.println(username1 + " is not existed!");
+        }
+        //checks username2
+        else if(!isExists(username2)){
+            //message
+            System.out.println(username2 + " is not existed!");
         }
 
-        else
-            System.out.println("SignIn First!");
-
+        else {
+            //reads file
+            Object Temp1 = objectFileReader(path + "\\" + username1 + ".bin");
+            //casting
+            Account account1 = null;
+            //sets data
+            account1 = (Account) Temp1;
+            //checks user1 state
+            if (account1.isSignedIn()) {
+                //reads file
+                Object Temp2 = objectFileReader(path + "\\" + username2 + ".bin");
+                //casting
+                Account account2 = (Account) Temp2;
+                //searches whether username 1 has already followed username 2
+                if(Search(account1.getFollowings(), account2.getUsername())){
+                    //message
+                    System.out.println("You have already followed " + username2 + " !");
+                }
+                else {
+                    //add following for user1
+                    account1.addFollowing(username2);
+                    //add follower for user2
+                    account2.addFollower(username1);
+                    //writes user1
+                    objectFileWriter(path + "\\" + username1 + ".bin", account1);
+                    //writes user2
+                    objectFileWriter(path + "\\" + username2 + ".bin", account2);
+                    //message
+                    System.out.println("Now you are following " + username2 + " !");
+                }
+            }
+            if (!account1.isSignedIn())
+                //message
+                System.out.println("SignIn First!");
+        }
     }
 
     /**
@@ -101,40 +125,118 @@ public class Observer implements AccountIOInterface {
      * @param username2
      */
     public void unFollow(String username1, String username2){
-        //temp object1
-        Object Temp1 = null;
-        //temp object2
-        Object Temp2 = null;
-        //account1
-        Account account1 = null;
-        //account2
-        Account account2 = null;
-        //reads file
-        Temp1 = objectFileReader(path + "/" + username1 + ".bin");
-        //casting
-        account1 = (Account) Temp1;
-        //checks user1 state
-        if(account1.isSignedIn()){
+            //checks first username
+            if (!isExists(username1)) {
+                //message
+                System.out.println(username1 + "is not Existed");
+            }
+            //checks second username
+            else if (!isExists(username2)) {
+                //message
+                System.out.println(username2 + "is not Existed");
+            }
+            else {
+                //reads file
+                Object Temp1 = objectFileReader(path + "\\" + username1 + ".bin");
+                //casting
+                Account account1 = null;
+                //sets data
+                account1 = (Account) Temp1;
+                //checks user1 state
+                if (account1.isSignedIn()) {
+                    //reads file
+                    Object Temp2 = objectFileReader(path + "\\" + username2 + ".bin");
+                    //casting
+                    Account account2 = (Account) Temp2;
+                    //searches whether username 1 has already followed username 2
+                    if(!Search(account1.getFollowings(), account2.getUsername())){
+                        //message
+                        System.out.println("You have not followed " + username2 + " !");
+                    }
+                    else {
+                        //removes following for user1
+                        account1.removeFollowing(username2);
+                        //removes follower for user2
+                        account2.removeFollower(username1);
+                        //writes user1
+                        objectFileWriter(path + "\\" + username1 + ".bin", account1);
+                        //writes user2
+                        objectFileWriter(path + "\\" + username2 + ".bin", account2);
+                        //message
+                        System.out.println(username2 + "is now omitted from your followings!");
+                    }
 
-            //reads file
-            Temp2 = objectFileReader(path + "/" + username2 + ".bin");
-            //casting
-            account2 = (Account) Temp2;
-            //removes following for user1
-            account1.removeFollowing(username2);
-            //removes follower for user2
-            account2.removeFollower(username1);
-            //writes user1
-            objectFileWriter(path + "/" + username1 + ".bin", account1);
-            //writes user2
-            objectFileWriter(path + "/" + username2 + ".bin", account2);
-
+                } else
+                    //message
+                    System.out.println("SignIn First!");
+            }
         }
 
-        else
-            System.out.println("SignIn First!");
 
+    /**
+     * deleteFollower method deletes username2 from followers of username1
+     * @param username1
+     * @param username2
+     */
+    public void deleteFollower(String username1, String username2){
+        //checks first username
+        if (!isExists(username1)) {
+            //message
+            System.out.println(username1 + "is not Existed");
+        }
+        //checks second username
+        else if (!isExists(username2)) {
+            //message
+            System.out.println(username2 + "is not Existed");
+        }
+        else{
+            //reads file
+            Object Temp1 = objectFileReader(path + "\\" + username1 + ".bin");
+            //casting
+            Account account1 = null;
+            //sets data
+            account1 = (Account) Temp1;
+            //checks user1 state
+            if (account1.isSignedIn()) {
+                //reads file
+                Object Temp2 = objectFileReader(path + "\\" + username2 + ".bin");
+                //casting
+                Account account2 = (Account) Temp2;
+                //searches whether username 1 has already followed username 2
+                if(!Search(account1.getFollowers(), username2)) {
+                    //message
+                    System.out.println(username2 + "You has not followed you!");
+                }
+                else {
+                    //removes follower for user1
+                    account1.removeFollower(username2);
+                    //removes following for user2
+                    account2.removeFollowing(username1);
+                    //writes user1
+                    objectFileWriter(path + "\\" + username1 + ".bin", account1);
+                    //writes user2
+                    objectFileWriter(path + "\\" + username2 + ".bin", account2);
+                    //message
+                    System.out.println(username2 + "is now omitted from your followers!");
+                }
+            }
+            else
+                //message
+                System.out.println("SignIn First!");
+        }
     }
+
+    public void Tweets(String username){
+        //checks first username
+        if (!isExists(username)) {
+            //message
+            System.out.println(username + "is not Existed");
+        }
+        else{
+
+        }
+    }
+
 
     /**
      * objectFileWriter method writes new byte base files
@@ -179,5 +281,76 @@ public class Observer implements AccountIOInterface {
         return account;
     }
 
-}
+    /**
+     * getUsers method reads usernames from file
+     * @return usernames
+     */
+    public ArrayList<String> getUsers() {
 
+        //Represents usernames
+        ArrayList<String> list = new ArrayList<>();
+
+        try {
+
+            //new scanner
+            Scanner s = new Scanner(new File(path + "\\usernames.txt"));
+            //output arraylist
+            list = new ArrayList<String>();
+            while (s.hasNext()) {
+                list.add(s.next());
+            }
+            s.close();
+
+            //IOException
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //returns arraylist
+        return list;
+    }
+
+    /**
+     * isExists searches among usernames
+     * @param username
+     * @return true/false
+     */
+    public boolean isExists(String username){
+
+        //Output variable
+        boolean Exists = false;
+        //searches on usernames
+        for(String UserName: getUsers()){
+            //checks whether usernames are equal
+            if(username.equals(UserName)){
+                Exists = true;
+                break;
+            }
+        }
+        //Returns output variable
+        return Exists;
+    }
+
+    /**
+     * Search method searches within an arrayList and checks whether a username exists or not
+     * @param UserNames
+     * @param username
+     * @return true/false
+     */
+    public boolean Search(ArrayList<String> UserNames, String username){
+
+      //Output variable
+      boolean Exists = false;
+      //Searches within usernames
+      for(String Username : UserNames){
+          //Checks
+          if(username.equals(Username))
+              Exists = true;
+              break;
+      }
+        //Returns output variable
+        return Exists;
+    }
+
+
+}
